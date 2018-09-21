689aa3e92400e0b3bd33f7f6a2835d44413b0054
Asrul H <talkasrul@gmail.com>
2018-09-22 04:25:03 +0700

    Config firebase remove

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 var config = {
-  apiKey: 'AIzaSyC6azX51_Tcv09FiE6vU3N7Z5HyprkwV08',
-  authDomain: 'kanban-f157d.firebaseapp.com',
-  databaseURL: 'https://kanban-f157d.firebaseio.com',
-  projectId: 'kanban-f157d',
-  storageBucket: 'kanban-f157d.appspot.com',
-  messagingSenderId: '1084929392663'
+  apiKey: '?',
+  authDomain: '?',
+  databaseURL: '?',
+  projectId: '?',
+  storageBucket: '?',
+  messagingSenderId: '?'
 }
 firebase.initializeApp(config)
 
